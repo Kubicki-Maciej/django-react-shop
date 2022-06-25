5bfe63ccd94f126f66e75f4fd790883feaf7bbe5
Maciej Kubicki <dev.kubicki@gmail.com>
2022-06-25 02:14:28 +0200

    created user register + autentication changed user serializer now have new attributes and changed returns, created views that get_users with permission and get_user_profile for single user

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path 
 from . import views
 
+# JWT 
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+# )
+
+
 urlpatterns = [
+    path('user/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/register/', views.register_user, name="register_user"),
+    path('users/profile/', views.get_user_profile, name="get_user_profile"),
+    path('users/', views.get_users, name="get_users"),
+
     path('', views.getRoutes, name="routes"),
+    
     path('products/', views.get_products, name="get_products"),
-    path('products/<str:pk>', views.get_single_product, name="get_single_product")
+    path('products/<str:pk>', views.get_single_product, name="get_single_product"),
+
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,17 +1,44 @@
+#Django
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.hashers import make_password
 
 # Imported model
 from .product import products
 from .models import Product
-
+from django.contrib.auth.models import User
 # RestFramework
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+# handler to custom message
+from rest_framework import status
 
 # Serializer 
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 
+# JWT 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -22,8 +49,9 @@ def getRoutes(request):
         ]
     return Response(routes)
 
-
 # Response dziala tylko wtedy kiedy ma dekorator api_view. https://www.django-rest-framework.org/api-guide/views/#api_view
+
+# PRODUCTS
 
 
 @api_view(['GET'])
@@ -34,7 +62,6 @@ def get_products(request):
     return Response(serializer.data)
 
 
-
 @api_view(['GET'])
 def get_single_product(request, pk):
     product = Product.objects.get(_id=pk)
@@ -42,4 +69,39 @@ def get_single_product(request, pk):
     return Response(serializer.data)
 
 
+# USERS
+
+
+@api_view(['POST'])
+def register_user(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail' : 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    """ return list of products"""
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
